39a273c60361876d692b6e471a8265748384b4ea
austinjaro <ajarolimek@nevada.unr.edu>
2023-04-16 20:18:35 -0700

    Door and Key working
    - got a working door with a key
    - also made a canvas to show key count and prompts to open door.

--- UCDN/Assets/Scripts/Door.cs
+++ UCDN/Assets/Scripts/Door.cs
@@ -2,48 +2,107 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Door : MonoBehaviour
 {
+    private PickupMgr pickup_script;
 
+    public GameObject keys;
     public GameObject movingDoor;
+    public GameObject LockedPanel = null;
+    public GameObject UseKeyPanel = null;
 
     public float maxOpening = -2.5f;
-
+    public float maxClosed = 2.5f;
     public float movementSpeed = -1f;
+
     bool playerIsHere;
+    bool open;
     bool opening;
+    bool keyAvailable;
 
     // Start is called before the first frame update
     void Start()
     {
+        pickup_script = keys.GetComponent<PickupMgr>();
         playerIsHere = false;
-        opening = false;
+       // opening = false;
+        keyAvailable = false;
+        LockedPanel.SetActive(false);
+        UseKeyPanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if((Input.GetKeyUp(KeyCode.E)))
+        if (pickup_script.KeyAmount > 0)
         {
-            opening = true;
+            keyAvailable = true;
+        }
+        else
+        {
+            keyAvailable = false;
         }
 
-        if (playerIsHere && opening)
+        if (playerIsHere)
         {
-            if(movingDoor.transform.position.y > maxOpening)
+            if (keyAvailable)
+            {
+                if ((Input.GetKeyDown(KeyCode.E)))
+                {
+                    pickup_script.KeyAmount--;
+                    open = true;
+                }
+            }
+
+        }
+
+        if (open)
+        {
+            if (movingDoor.transform.position.y > maxOpening)
             {
                 movingDoor.transform.Translate(0f, movementSpeed * Time.deltaTime, 0f);
             }
         }
+        if (movingDoor.transform.position.y <= maxOpening)
+        {
+            open = false;
+            LockedPanel.SetActive(false);
+            UseKeyPanel.SetActive(false);
+        }
     }
 
-    
+
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+
+        if (other.gameObject.tag == "Player")
         {
             playerIsHere = true;
+
+            if (keyAvailable)
+            {
+                UseKeyPanel.SetActive(true);
+                LockedPanel.SetActive(false);
+            }
+            else
+            {
+                LockedPanel.SetActive(true);
+                UseKeyPanel.SetActive(false);
+            }
+
         }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        { 
+            playerIsHere = false;
+            LockedPanel.SetActive(false);
+            UseKeyPanel.SetActive(false);
+        }
+    }
+    
 }
--- UCDN/Assets/Scripts/Managers/PickupMgr.cs
+++ UCDN/Assets/Scripts/Managers/PickupMgr.cs
@@ -1,16 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PickupMgr : MonoBehaviour
 {
-	
+    //private Door door_script;
+
+   // public GameObject KeyCount;
+    public int KeyAmount = 0;
+    public TextMeshProUGUI keyCount;
+
+
+    void Start()
+    {
+        SetKeyCountText();
+        //door_script = KeyCount.GetComponent<Door>();
+    }
+
+    void Update()
+    {
+        SetKeyCountText();
+       /* if (door_script.open)
+        {
+            KeyAmount--;
+        }*/
+    }
+
+
+
+    private void SetKeyCountText()
+    {
+        keyCount.text = "Keys: " + KeyAmount.ToString();
+
+    }
+
 	private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("key1"))
         {   
             other.gameObject.SetActive(false);
-          
+            KeyAmount++;
+            SetKeyCountText();
         }
 	}	
 }
